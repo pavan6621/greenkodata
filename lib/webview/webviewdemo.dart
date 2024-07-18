c608eea03179809d44e7915b9e145f3fe292babc
pavan6621 <92384918+pavan6621@users.noreply.github.com>
2024-07-18 14:56:36 +0530

    Update webviewdemo.dart

--- lib/webview/webviewdemo.dart
+++ lib/webview/webviewdemo.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
 class WebViewDemo extends StatefulWidget {
   @override
@@ -8,25 +8,66 @@ class WebViewDemo extends StatefulWidget {
 }
 
 class _WebViewDemoState extends State<WebViewDemo> {
+  bool _showWebView = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _delayedShowWebView();
+  }
+
+  void _delayedShowWebView() {
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        _showWebView = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: PreferredSize(
-        preferredSize: Size.fromHeight(3.0),
-        child: AppBar(
-          backgroundColor: Colors.black,
-          title: Text(''),
+    return  _showWebView 
+          ?  Scaffold(
+      appBar: AppBar(
+              title: Text('Toolbar'),
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {},
+                ),
+              ],
+            ) ,
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: const WebView(
+                  initialUrl: "https://www.greenkogroup.com/about.php#company",
+                  javascriptMode: JavascriptMode.unrestricted,
+                ),
+              
         ),
       ),
+    ): Scaffold(
+    
       body: Center(
-            child: Container(
-                color: Colors.blue,
-                child: const WebView(
-                initialUrl: "https://www.greenkogroup.com/about.php#company",
-                javascriptMode: JavascriptMode.unrestricted,
-              ),
-              ),
-          ),
+        child: Container(
+          color: Colors.white,
+          child: const Center(
+                  child: Text(
+                    'Loading Page...',
+                    style: TextStyle(color: Color.fromARGB(255, 7, 18, 218), fontSize: 24),
+                  ),
+                ),
+        ),
+      ),
     );
   }
 }
